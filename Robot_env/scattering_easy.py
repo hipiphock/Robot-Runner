cf0fec2ba984af5c15089492f7b3f6e57885090b
sp9103 <sp9103@naver.com>
2020-09-08 13:37:56 +0900

    finished scattering_easy

--- Robot_env/scattering_easy.py
+++ Robot_env/scattering_easy.py
@@ -1,11 +1,11 @@
 from scipy.spatial import distance as dist
 import imutils
-from imutils import perspective
 from imutils import contours
 import numpy as np
-import argparse
 import cv2
-
+import copy
+import logging
+from Robot_env.config import RL_Obj_List
 
 """
 scattering을 하기 위해서는 무엇을 해야할까?
@@ -30,118 +30,149 @@ Logistics
 2. 
 """
 
+scattering_logger = logging.getLogger("scattering_easy")
+
+
 def get_neighbor_obj():
     # 어떻게 detect 해야할까?
     # object의 경계선은 어디에 있지?
     return [1, 2, 3]
+
 
 def put_neighbor_obj(obj):
     # neighboring object 옮기기
     # 어떻게 옮기지?
     pass
 
+
 def scatter():
     neighbor_obj_list = get_neighbor_obj()
     for neighbor_obj in neighbor_obj_list:
         put_neighbor_obj(neighbor_obj)
 
+
 # original contents from:
 # https://www.pyimagesearch.com/2016/04/04/measuring-distance-between-objects-in-an-image-with-opencv/
 def midpoint(ptA, ptB):
-	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
 
-def get_distance(obj_1, obj_2):
+def get_distance(image, robot, detected_obj_lists):
     """
-    Get the distance between object1 and object2.
+    Get the distance between objects
     TODO:
     1. Split the function to object detector and distance computer.
     2. Specify the real distance between two objects.
-    3. REMOVE INCHES.
+    3. Identify objects! (IMPORTANT)
     """
+    # distance_array is an array that represents distance between two objects.
+    # For example, if cup_red(9) and big_box(12)'s distance is 11.82, then distance_aray[9][12] = 11.82.
+    distance_array = np.ones((50, 50))
+    distance_array = np.full_like(distance_array, 999.99)
+
     # load the image, convert it to grayscale, and blur it slightly
-    image = cv2.imread("C:/Users/incorl_robot/Desktop/2020PartTimeJob/Robot-Runner/Robot_env/test_image.png")    # In this case, it should be image from
-                                            # Intel realsense camera.
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+    # gray = cv2.GaussianBlur(gray, (7, 7), 0)
     # perform edge detection, then perform a dilation + erosion to
     # close gaps in between object edges
+    # TODO: 기존 코드에서 object 경계 가져올 것 ?
     edged = cv2.Canny(gray, 50, 100)
-    edged = cv2.dilate(edged, None, iterations=1)
-    edged = cv2.erode(edged, None, iterations=1)
+    # edged = cv2.dilate(edged, None, iterations=1)
+    # edged = cv2.erode(edged, None, iterations=1)
+
     # find contours in the edge map
     cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     # sort the contours from left-to-right and, then initialize the
     # distance colors and reference object
     (cnts, _) = contours.sort_contours(cnts)
-    colors = ((0, 0, 255), (240, 0, 159), (0, 165, 255), (255, 255, 0),
-        (255, 0, 255))
-    refObj = None
 
     # loop over the contours individually
-    for c in cnts:
+    cnt_check_list = [False for _ in range(len(cnts))]
+    mod_image = copy.deepcopy(image)
+    i, j = 0, 0
+    # 두 object를 선택한 후, 해당 object를 감싸는 가장 작은 원 사이의 거리를 구한다.
+    # 해당 거리를 distance_array에 넣는다.
+    for obj1 in cnts:
+        cnt_check_list[i] = True
         # if the contour is not sufficiently large, ignore it
-        if cv2.contourArea(c) < 100:    # This should be changed with relate to
-            continue                    # robot environment.
+        if cv2.contourArea(obj1) < 80:  # This should be changed with relate to
+            continue  # robot environment.
         # compute the rotated bounding box of the contour
-        box = cv2.minAreaRect(c)
-        box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-        box = np.array(box, dtype="int")
-        # order the points in the contour such that they appear
-        # in top-left, top-right, bottom-right, and bottom-left
-        # order, then draw the outline of the rotated bounding
-        # box
-        box = perspective.order_points(box)
-        # compute the center of the bounding box
-        cX = np.average(box[:, 0])
-        cY = np.average(box[:, 1])
-
-        # if this is the first contour we are examining (i.e.,
-        # the left-most contour), we presume this is the
-        # reference object
-        if refObj is None:
-            # unpack the ordered bounding box, then compute the
-            # midpoint between the top-left and top-right points,
-            # followed by the midpoint between the top-right and
-            # bottom-right
-            (tl, tr, br, bl) = box
-            (tlblX, tlblY) = midpoint(tl, bl)
-            (trbrX, trbrY) = midpoint(tr, br)
-            # compute the Euclidean distance between the midpoints,
-            # then construct the reference object
-            D = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
-            refObj = (box, (cX, cY), D / 600)   # 600 is the width of test_image.png
-            continue
-
-        # draw the contours on the image
-        orig = image.copy()
-        cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
-        cv2.drawContours(orig, [refObj[0].astype("int")], -1, (0, 255, 0), 2)
-        # stack the reference coordinates and the object coordinates
-        # to include the object center
-        refCoords = np.vstack([refObj[0], refObj[1]])
-        objCoords = np.vstack([box, (cX, cY)])
+        (obj1_x, obj1_y), obj1_radius = cv2.minEnclosingCircle(obj1)
+        obj1_center = (int(obj1_x), int(obj1_y))
+        obj1_radius = int(obj1_radius)
+        cv2.circle(mod_image, obj1_center, obj1_radius, (177, 177, 177), 1)
 
         # TODO: getting the nearest distance between two objects
         # loop over the original points
-        for ((xA, yA), (xB, yB), color) in zip(refCoords, objCoords, colors):
-            # draw circles corresponding to the current points and
-            # connect them with a line
-            cv2.circle(orig, (int(xA), int(yA)), 5, color, -1)
-            cv2.circle(orig, (int(xB), int(yB)), 5, color, -1)
-            cv2.line(orig, (int(xA), int(yA)), (int(xB), int(yB)),
-                color, 2)
-            # compute the Euclidean distance between the coordinates,
-            # and then convert the distance in pixels to distance in
-            # units
-            D = dist.euclidean((xA, yA), (xB, yB)) / refObj[2]
-            (mX, mY) = midpoint((xA, yA), (xB, yB))
-            cv2.putText(orig, "{:.1f}in".format(D), (int(mX), int(mY - 10)),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.55, color, 2)
-            # show the output image
-            cv2.imshow("Image", orig)
+        color = (223, 223, 223)
+        orig = image.copy()
+        j = 0
+        for obj2 in cnts:
+            if cnt_check_list[j] == True:
+                j += 1
+                continue
+            if cv2.contourArea(obj2) < 80:
+                j += 1
+                continue
+            (obj2_x, obj2_y), obj2_radius = cv2.minEnclosingCircle(obj2)
+            obj2_center = (int(obj2_x), int(obj2_y))
+            obj2_radius = int(obj2_radius)
+            cv2.circle(mod_image, obj2_center, obj2_radius, (177, 177, 177), 1)
+
+            # TODO: draw line between two intersection point
+            line_tangent = (obj2_center[1]-obj1_center[1]) / (obj2_center[0]-obj1_center[0])
+            inter1_plus_x = obj1_center[0] + np.sqrt(obj1_radius*obj1_radius / (1+line_tangent*line_tangent))
+            inter1_plus_y = (inter1_plus_x-obj1_center[0]) * line_tangent + obj1_center[1]
+            inter1_minus_x = obj1_center[0] - np.sqrt(obj1_radius*obj1_radius / (1+line_tangent*line_tangent))
+            inter1_minus_y = (inter1_minus_x-obj1_center[0]) * line_tangent + obj1_center[1]
+
+            inter2_plus_x = obj2_center[0] + np.sqrt(obj2_radius * obj2_radius / (1 + line_tangent * line_tangent))
+            inter2_plus_y = (inter2_plus_x-obj2_center[0]) / line_tangent + obj2_center[1]
+            inter2_minus_x = obj2_center[0] - np.sqrt(obj2_radius * obj2_radius / (1 + line_tangent * line_tangent))
+            inter2_minus_y = (inter2_minus_x-obj2_center[0]) * line_tangent + obj2_center[1]
+
+            small_x = min(obj1_center[0], obj2_center[0])
+            big_x = max(obj1_center[0], obj2_center[0])
+            small_y = min(obj1_center[1], obj2_center[1])
+            big_y = max(obj1_center[1], obj2_center[1])
+            inter1, inter2 = (0, 0), (0, 0)
+            if small_x <= inter1_minus_x <= big_x:
+                inter1 = (int(inter1_minus_x), int(inter1_minus_y))
+            else:
+                inter1 = (int(inter1_plus_x), int(inter1_plus_y))
+            if small_x <= inter2_minus_x <= big_x:
+                inter2 = (int(inter2_minus_x), int(inter2_minus_y))
+            else:
+                inter2 = (int(inter2_plus_x), int(inter2_plus_y))
+            D = dist.euclidean(obj1_center, obj2_center) - obj1_radius - obj2_radius
+            cv2.line(mod_image, inter1, inter2, color)
+            (mid_x, mid_y) = midpoint(obj1_center, obj2_center)
+            cv2.putText(mod_image, "{:.2f}".format(D), (int(mid_x), int(mid_y)), cv2.FONT_HERSHEY_SIMPLEX, 0.55, color,
+                        2)
+            cv2.imshow("distance between objects", mod_image)
+
+            # SETTING INDEX
+            # TODO: specify which object it is - and set index to those objects
+            # If obj1's center's color is same as detected_obj_lists
+            obj1_idx, obj2_idx = 0, 0
+            for obj_num in detected_obj_lists:
+                if tuple(mod_image[obj1_center[1]][obj1_center[0]]) == RL_Obj_List[obj_num][1]:
+                    obj1_idx = obj_num
+            for obj_num in detected_obj_lists:
+                if tuple(mod_image[obj2_center[1]][obj2_center[0]]) == RL_Obj_List[obj_num][1]:
+                    obj2_idx = obj_num
+            distance_array[obj1_idx][obj2_idx] = D
+            distance_array[obj2_idx][obj1_idx] = D
+            scattering_logger.info("distance between {} and {} is {}.".format(obj1_idx, obj2_idx, D))
             cv2.waitKey(0)
+            j += 1
+        i += 1
+
+    return distance_array
+
 
 if __name__ == "__main__":
-    get_distance(1, 2)
+    image = None
+    get_distance(image)
